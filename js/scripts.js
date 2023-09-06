e79288f3433f60723c4bfaf65d219047acfb00c3
chakirach94 <95524156+chakirach94@users.noreply.github.com>
2023-09-06 17:45:15 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,35 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+let countdown = 10 * 60; // 10 minutes in seconds
+const timerElement = document.getElementById('timer');
+
+function updateTimerDisplay() {
+    const minutes = Math.floor(countdown / 60);
+    const seconds = countdown % 60;
+    timerElement.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
+
+setInterval(() => {
+    if (countdown > 0) {
+        countdown--;
+        updateTimerDisplay();
+    }
+}, 1000);
+function showText(index) {
+    const cards = document.querySelectorAll('.card');
+    const textElement = cards[index].querySelector('.card-text');
+    textElement.style.display = 'block';
+}
+
+function toggleText(index) {
+    const cards = document.querySelectorAll('.card');
+    const textElement = cards[index].querySelector('.card-text');
+
+    if (textElement.style.display === 'block') {
+        textElement.style.display = 'none';
+    } else {
+        textElement.style.display = 'block';
+    }
+}
